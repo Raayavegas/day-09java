f828c6440846d0d577f02447895346e6eabf45ff
Veerala Rithika <veeralarithika@gmail.com>
2024-12-10 11:23:13 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class D
+{
+    public static void main(String args[])
+    {
+        String a="hello";
+        String b=new String("hai");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
